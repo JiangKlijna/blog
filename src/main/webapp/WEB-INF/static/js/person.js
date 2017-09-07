850dca55bd045234137667206e4b955a669e39aa
jiangKlijna <leil729564090@outlook.com>
2017-09-07 16:32:28 +0800

    :fire:person.do display articles

--- src/main/webapp/WEB-INF/static/js/person.js
+++ src/main/webapp/WEB-INF/static/js/person.js
@@ -1,10 +1,13 @@
-
 window.cj = {
+    ARTICLE_LIST_URL: "article/listByUser.json",
     FOLLOW_USER_URL: "user/follow.json",
     onLoad: function (){
+        if (!isExist) return;
+        cj.onLoadMore();
+        $('#load_more_btn').click(cj.onLoadMore);
 
-
-        $('.follow').click(cj.onClickFollow);
+        $(document).on("click", ".follow", cj.onClickFollow);
+        $(document).on("click", ".article .pointer", cj.openArticle);
     },
     //当点击关注的时候
     onClickFollow: function (){
@@ -23,7 +26,66 @@ window.cj = {
                 }
             } else dialog.info(result.message);
         });
-
+    },
+    // 0 is 文章 1 is 粉丝 2 is 关注
+    type: 0,
+    // 当前页数
+    pageNum: 0,
+    // 每页多少
+    perPage: 10,
+    // 加载更多
+    onLoadMore: function() {
+        if (cj.type == 0) { // 加载文章
+            $.post(cj.ARTICLE_LIST_URL, {'username': name, 'perPage': cj.perPage, 'pageNum': cj.pageNum}, function (result){
+                if (result.code == 0) cj.displayArticles(result.data);
+                else dialog.info(result.message);
+            });
+        } else if (cj.type == 1) { // 加载粉丝
+        } else { // 加载关注
+        }
+    },
+    // 显示文章列表
+    displayArticles(articles) {
+        if (articles.length == 0) {
+            $('#loadmore').hide();
+            return;
+        }
+        cj.pageNum++;
+        var $articles = $('#listview');
+        for (var i in articles) {
+            var html = cj.articleHtml(articles[i]);
+            $articles.append(html);
+        }
+        if (articles.length < cj.perPage) { // 如果文章数小于期望的数量则隐藏加载更多
+            $('#loadmore').hide();
+            return;
+        }
+    },
+    // 获得文章的html
+    articleHtml(a) {
+        return "<div class=\"article\" data-id=\"" + a.id
+            + "\"><p><span class=\"atitle pointer\">" + a.title
+            + "<span></p><p><span class=\"content pointer\">"
+            + a.preview + "</span><span class=\"readmore pointer\">...查看全文</span></p><p><span class=\"username\"><a href=\"person.do?name="
+            + a.username + "\">" + a.username
+            + "</a></span><span class=\"time\">"
+            + cj.timestampToString(a.createtime)
+            + "</span><span class=\"right pointer\">" + a.numberOfComments
+            + "条评论</span><span class=\"right pointer\">" + a.favoritenumber
+            + " 赞</span></p><hr></div>";
+    },
+    // 时间戳转换字符串
+    timestampToString(time) {
+        var d = new Date(time);
+        return (d.getYear() + 1900) + "-" +
+                (d.getMonth() + 1) + "-" +
+                d.getDate() + " " +
+                d.getHours() + ":" +
+                d.getMinutes();
+    },
+    // 打开article
+    openArticle() {
+        location = "article.do?id=" + $(this).parent().parent().data("id");
     },
 }
 $(cj.onLoad)
--- src/main/webapp/WEB-INF/views/person.jsp
+++ src/main/webapp/WEB-INF/views/person.jsp
@@ -19,14 +19,18 @@
             <button class="follow" data-name="${vu.username}"><c:choose><c:when test="${isFollow}">取消关注</c:when><c:otherwise>关注</c:otherwise></c:choose></button>
         </p>
     </c:when></c:choose>
-    <p class="text-center author">
-        <span class="label label-primary">关注:${vu.numberOfConcerns}</span>
-        <span class="label label-info">粉丝:${vu.numberOfFans}</span>
-        <span class="label label-warning">文章:${vu.numberOfArticles}</span>
+    <p class="text-center">
+        <span class="label label-primary pointer">文章:${vu.numberOfArticles}</span>
+        <span class="label label-info pointer">粉丝:${vu.numberOfFans}</span>
+        <span class="label label-warning pointer">关注:${vu.numberOfConcerns}</span>
         <span class="label label-success">喜欢:${vu.favoriteNumber}</span>
         <span class="label label-danger">字数:${vu.numberOfWords}</span>
     </p>
     <hr>
+    <div id="listview"></div>
+    <div id="loadmore">
+        <p id="load_more_btn" class="text-center pointer">查看更多</p><hr>
+    </div>
     </c:when>
     <c:otherwise>
         <p class="text-center" style="font-weight: 900; font-size: 20px;padding: 20px;">此用户不存在, <a href="./" class="text-primary">返回主页</a></p>
@@ -35,6 +39,7 @@
 </section>
 <%@ include file="footer.jsp" %>
 <%@ include file="dialog.jsp" %>
+<script>isExist = ${isExist};name = "${name}"</script>
 <script src="static/js/person.js"></script>
 </body>
 </html>
