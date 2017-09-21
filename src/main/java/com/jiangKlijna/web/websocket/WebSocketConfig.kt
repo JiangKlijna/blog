e7e897cd659fa465916a16304487fd88d26ac64d
jiangKlijna <leil729564090@outlook.com>
2017-09-21 10:59:46 +0800

    :fire:config ChatWebSocketHandler

--- src/main/java/com/jiangKlijna/web/websocket/WebSocketConfig.kt
+++ src/main/java/com/jiangKlijna/web/websocket/WebSocketConfig.kt
@@ -9,6 +9,7 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor
+import javax.annotation.Resource
 
 /**
  * Created by jiangKlijna on 9/20/2017.
@@ -17,9 +18,12 @@ import org.springframework.web.socket.server.support.HttpSessionHandshakeInterce
 @EnableWebSocket
 open class WebSocketConfig : WebSocketConfigurer {
 
+	@Resource(name = "ChatWebSocketHandler")
+	var handler: ChatWebSocketHandler? = null
+
 	override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
-		println("registerWebSocketHandlers")
-		registry.addHandler(myHandler(), "/echo.ws").let {
+		println("registerWebSocketHandlers" + handler)
+		registry.addHandler(handler, "/echo.ws").let {
 			//            it.withSockJS()
 			it.addInterceptors(HandshakeInterceptor())
 			it.setAllowedOrigins("http://localhost")
@@ -27,16 +31,11 @@ open class WebSocketConfig : WebSocketConfigurer {
 	}
 
 	@Bean
-	open fun myHandler(): WebSocketHandler {
-		return ChatWebSocketHandler()
-	}
-
 	open fun HandshakeInterceptor() = object : HttpSessionHandshakeInterceptor() {
 
 		@Throws(Exception::class)
-		override fun beforeHandshake(request: ServerHttpRequest?,
-									 response: ServerHttpResponse?, wsHandler: WebSocketHandler?,
-									 attributes: Map<String, Any>): Boolean {
+		override fun beforeHandshake(request: ServerHttpRequest?, response: ServerHttpResponse?,
+									 wsHandler: WebSocketHandler?, attributes: Map<String, Any>): Boolean {
 			// 解决The extension [x-webkit-deflate-frame] is not supported问题
 			if (request!!.headers.containsKey("Sec-WebSocket-Extensions")) {
 				request.headers.set("Sec-WebSocket-Extensions", "permessage-deflate")
