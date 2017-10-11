79153be3c9d33c108a89e0551e4fc710299a8a9e
jiangKlijna <leil729564090@outlook.com>
2017-10-11 15:55:50 +0800

    :fire:index.js

--- src/main/webapp/WEB-INF/static/js/index.js
+++ src/main/webapp/WEB-INF/static/js/index.js
@@ -1,7 +1,39 @@
 window.cj = {
-
-
+    LIST_ARTICLE_INDEX_URL: '',
+    LIST_SUBJECT_INDEX_URL: '',
+    LIST_ARTICLE_FOLLOW_URL: '',
+    LIST_SUBJECT_FOLLOW_URL: '',
+    LIST_ARTICLE_SEARCH_URL: '',
+    LIST_SUBJECT_SEARCH_URL: '',
+    // 获得文章列表的url
+    getListArticleUrl: function() {
+        if (type === 'index')
+            return cj.LIST_ARTICLE_INDEX_URL;
+        else if (type === 'follow')
+            return cj.LIST_ARTICLE_FOLLOW_URL;
+        else
+            return cj.LIST_ARTICLE_SEARCH_URL + type;
+    },
+    // 获得主题列表的url
+    getListSubjectUrl: function() {
+        if (type === 'index')
+            return cj.LIST_SUBJECT_INDEX_URL;
+        else if (type === 'follow')
+            return cj.LIST_SUBJECT_FOLLOW_URL;
+        else
+            return cj.LIST_SUBJECT_SEARCH_URL + type;
+    },
+    // 当前页数 article
+    a_pageNum: 0,
+    // 每页多少 article
+    a_perPage: 10,
+    // 当前页数 subject
+    s_pageNum: 0,
+    // 每页多少 subject
+    s_perPage: 10,
     onLoad: function () {
+        cj.onClickArticleLoadMore();
+        cj.onClickSubjectLoadMore();
         $('#article_load_more').click(cj.onClickArticleLoadMore);
         $('#subject_load_more').click(cj.onClickSubjectLoadMore);
     },
@@ -10,6 +42,15 @@ window.cj = {
     },
     // 当点击#subjects #subject_load_more
     onClickSubjectLoadMore: function () {
+    },
+    // 时间戳转换字符串
+    timestampToString(time) {
+        var d = new Date(time);
+        return (d.getYear() + 1900) + "-" +
+                (d.getMonth() + 1) + "-" +
+                d.getDate() + " " +
+                d.getHours() + ":" +
+                d.getMinutes();
     },
 }
 $(cj.onLoad)
