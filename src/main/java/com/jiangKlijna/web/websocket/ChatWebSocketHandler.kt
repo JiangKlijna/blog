19cbe16261c20682fa84278248d5a3f7e4012e69
jiangKlijna <leil729564090@outlook.com>
2017-11-03 17:43:18 +0800

    :fire:rewrite

--- src/main/java/com/jiangKlijna/web/websocket/ChatWebSocketHandler.kt
+++ src/main/java/com/jiangKlijna/web/websocket/ChatWebSocketHandler.kt
@@ -1,6 +1,6 @@
 package com.jiangKlijna.web.websocket
 
-import com.jiangKlijna.web.bean.User
+import com.jiangKlijna.web.bean.Message
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.web.socket.CloseStatus
 import org.springframework.web.socket.TextMessage
@@ -15,40 +15,31 @@ import javax.annotation.Resource
 class ChatWebSocketHandler : TextWebSocketHandler() {
 
 	@Resource(name = "redisTemplate")
-	var rt: RedisTemplate<String, User>? = null
+	var rt: RedisTemplate<String, Message>? = null
 
 	//接收文本消息，并发送出去
-	@Throws(Exception::class)
-	override fun handleTextMessage(session: WebSocketSession?, message: TextMessage?) {
-
-//        chatTextMessageHandler(message!!.payload)
-		super.handleTextMessage(session, message)
-		session!!.sendMessage(TextMessage("Server:connected OK!"))
+	override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
+		session.sendMessage(message)
 	}
 
 	//连接建立后处理
-	@Throws(Exception::class)
-	override fun afterConnectionEstablished(session: WebSocketSession?) {
+	override fun afterConnectionEstablished(session: WebSocketSession) {
 		sessions.add(session)
-		//处理离线消息
+		session.sendMessage(TextMessage("Server:connected OK!"))
 	}
 
 	//抛出异常时处理
-	@Throws(Exception::class)
-	override fun handleTransportError(session: WebSocketSession?, exception: Throwable?) {
-		if (session!!.isOpen) session.close()
+	override fun handleTransportError(session: WebSocketSession, exception: Throwable?) {
+		if (session.isOpen) session.close()
 		sessions.remove(session)
 	}
 
 	//连接关闭后处理
-	@Throws(Exception::class)
-	override fun afterConnectionClosed(session: WebSocketSession?, closeStatus: CloseStatus?) {
+	override fun afterConnectionClosed(session: WebSocketSession, closeStatus: CloseStatus?) {
 		sessions.remove(session)
 	}
 
-	override fun supportsPartialMessages(): Boolean {
-		return false
-	}
+	override fun supportsPartialMessages() = false
 
 	companion object {
 		private val sessions = Collections.synchronizedList(ArrayList<WebSocketSession>())
